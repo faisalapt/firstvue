cf6635633fd4ed93e75861cb9fc8cf03364848b5
faisalapt <faisal.aprianto@student.umn.ac.id>
2020-11-17 13:09:04 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -22,23 +22,59 @@ export default {
   },
   data(){
     return {
-      employees: [
-        {
-          id: 1,
-          name: 'Richard Hendricks',
-          email: 'richard@piedpiper.com',
-        },
-        {
-          id: 2,
-          name: 'Betram Gilfoyle',
-          email: 'gilfoyle@piedpiper.com',
-        },
-        {
-          id: 3,
-          name: 'Dinesh Chugtai',
-          email: 'dinesh@piedpiper.com',
-        },
-      ],
+      employees: [],
+    }
+  },
+  mounted() {
+    this.getEmployees()
+  },
+  async getEmployees() {
+    try{
+      const response = await fetch('https://jasonplaceholder.typicode.com/users')
+      const data = await response.json()
+      this.employees = data
+    }
+    catch(error){
+      console.error(error)
+    }
+  },
+  async addEmployee(employee){
+    try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users',{
+        method: 'POST',
+        body: JSON.stringify(employee),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      })
+      const data = await response.json()
+      this.employees = [...this.employees, data]
+    }
+    catch (error){
+      console.error(error)
+    }
+  },
+  async editEmployee(id, updateEmployee){
+    try{
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify(updateEmployee),
+        headers: { 'Content-type': 'application/json; charset=UTF-8'},
+      })
+      const data = await response.json()
+      this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
+    }
+    catch(error){
+      console.error(error)
+    }
+  },
+  async deleteEmployee(id){
+    try{
+      await fetch(`https://jsonplaceholder.typicode.com/users/${id}`,{
+        method:"DELETE"
+      });
+      this.employees = this.employees.filter(employee => employee.id !== id);
+    }
+    catch(error){
+      console.error(error);
     }
   },
   methods: {
